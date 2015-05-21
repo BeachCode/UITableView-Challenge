4197189e24a5a62d7a9073cb28c3d437049418d1
Dustin M <Eleuthera@Dustins-MacBook-Air.local>
2015-05-21 15:34:30 -0400

    added new class and table view controller

---
+++ UITableViewController Challenge Solution/TableViewControllerChallenge.h
@@ -0,0 +1,13 @@
+//
+//  TableViewControllerChallenge.h
+//  UITableViewController Challenge Solution
+//
+//  Created by Dustin M on 5/21/15.
+//  Copyright (c) 2015 Vento. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TableViewControllerChallenge : UITableViewController
+
+@end
